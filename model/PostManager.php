aaac528582736739b7e5bcf91eb63991b6f8e982
todaik <wbarla33@gmail.com>
2017-11-17 07:34:11 +0100

    ajout fonctionnalites post manager
    addpost updatepost, getpostvisible and getposts.
    Cela fonctionne

--- model/PostManager.php
+++ model/PostManager.php
@@ -1,30 +1,51 @@
 <?php
 class PostManager 
 {
-	public function add(post $post)
+	public function doAddPost($title,$abstract,$content)
 	{
-
-
-	}
-
-	public function update()
-	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('INSERT INTO post(title,content,abstract,datepost) VALUES (?,?,?,now())');
+		$req->execute(array($title,$abstract,$content));
 		
-
-
 	}
 
-	public function delete($id)
+	public function doUpdatePost($id,$newtitle,$newabstract,$newcontent,$newstatepost)
 	{
-
+		$db = $this->dbConnect();
+		$req = $db->prepare('UPDATE post SET title = :newtitle, content = :newcontent, abstract = :newabstract, dateupdatepost = NOW(),statepost = :newstatepost WHERE id =:id');
+		$req->execute(array(
+			'newtitle'=>$newtitle,
+			'newcontent'=>$newcontent,
+			'newabstract'=>$newabstract,
+			'newstatepost'=>$newstatepost,
+			'id'=>$id));
 	}
 
-	public function getPosts($offset, $limit)
+	public function doDeletePost($id)
+	{
+		$db = $this->dbConnect();
+		$message=$db->prepare('DELETE FROM post WHERE id=?');
+	    $message->execute(array($id));
+	}
+
+	public function getPostsVisible($offset, $limit)
 	{
 		$db = $this->dbConnect();
 		$offset = (int) $offset;
 	    $limit = (int) $limit; 
 		$req = $db->prepare('SELECT id, title, content, abstract,DATE_FORMAT(datepost, \'%d/%m/%Y à %Hh%imin%ss\') AS newdatepost FROM post WHERE statepost=\'visible\' ORDER BY datepost DESC LIMIT :offset, :limit');
+		$req->bindParam(':offset', $offset, PDO::PARAM_INT);
+	    $req->bindParam(':limit', $limit, PDO::PARAM_INT);
+		$req->execute();
+		return $req;
+	}
+
+		public function getPosts($offset, $limit)
+	{
+		$db = $this->dbConnect();
+		$offset = (int) $offset;
+	    $limit = (int) $limit; 
+		$req = $db->prepare('SELECT id, title, content, abstract,statepost,DATE_FORMAT(dateupdatepost, \'%d/%m/%Y à %Hh%imin%ss\') AS newdatepost FROM post ORDER BY dateupdatepost DESC LIMIT :offset, :limit');
 		$req->bindParam(':offset', $offset, PDO::PARAM_INT);
 	    $req->bindParam(':limit', $limit, PDO::PARAM_INT);
 		$req->execute();
