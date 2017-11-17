c01784a70d710c7099e8717ed9105cecddc74572
todaik <wbarla33@gmail.com>
2017-11-17 07:36:35 +0100

    poo baclend controller
    transformation du contorlleur en POO en cours

--- controler/backend.php
+++ controler/backend.php
@@ -1,5 +1,8 @@
 <?php
-require('model/backend.php');
+//require('model/backend.php');
+require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
+require_once('model/UserManager.php');
 
 
 function formConnect()
@@ -10,7 +13,8 @@ function formConnect()
 function logOn()
 {
 	
-	$posts = getPostsBack(0,20);
+	$PostManager = new PostManager();
+	$posts = $PostManager->getPosts(0,20);
 	require('view/backend/indexView.php');
 }
 
@@ -24,9 +28,9 @@ function logOff()
 
 function backPost()
 {
-	
+	$PostManager = new PostManager();
 	if (isset($_GET['id']) && $_GET['id']>0) {
-		$post = getPost($_GET['id']);
+		$post = $PostManager->getPost($_GET['id']);
 		require("view/backend/postView.php");
 	}
 	
@@ -34,16 +38,19 @@ function backPost()
 
 function updatePost()
 {
+	$PostManager = new PostManager();
 	if(isset($_POST['updatePost'])) {
-		doUpdatePost();
+		$post = $PostManager->doUpdatePost($_GET['id'],$_POST['newtitle'],$_POST['newAddAbstract'],$_POST['newcontent'],$_POST['newstatepost']);
 	}
 }
 
 function deletePost()
 {
+	$PostManager = new PostManager();
+	$CommentManager = new CommentManager();
 	if(isset($_POST['deletePost'])) {
-		doDeleteComments();
-		doDeletePost();
+		$comments = $CommentManager->doDeleteComments($_GET['id']);
+		$post = $PostManager->doDeletePost($_GET['id']);
 		header("Location:index.php?action=logon");
 	}
 }
@@ -52,8 +59,9 @@ function deletePost()
 
 function adminConnect()
 {
-	
-	if(isUserAdmin($_POST['pseudoconnect'],$_POST['mdpconnect'])) {
+	$UserManager = new UserManager();
+	$user = $UserManager->isUserAdmin($_POST['pseudoconnect'],$_POST['mdpconnect']);
+	if($user == TRUE) {
 		header("Location:index.php?action=logon");
 	}
 	else {
@@ -65,9 +73,10 @@ function adminConnect()
 function addPost()
 {
 	require("view/backend/addPostView.php");
+	$PostManager = new PostManager();
 	if(isset($_POST['addPost'])) {
-		if(!empty($_POST['addTitle']) && !empty($_POST['addContent'])) {
-			doAddPost();
+		if(!empty($_POST['addTitle']) && !empty($_POST['addContent']) && !empty($_POST['addAbstract'])) {
+			$post = $PostManager->doAddPost($_POST['addTitle'],$_POST['addAbstract'],$_POST['addContent']);
 			header("Location:index.php?action=logon");
 		}
 		else {
