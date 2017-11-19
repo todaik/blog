10f8b02fe1ce1b8e89d866bdc91a641709dd07c2
todaik <wbarla33@gmail.com>
2017-11-19 18:52:29 +0100

    Mise a jour barre menu backend
    changement de lien de la barre de manu du backend

--- view/backend/template.php
+++ view/backend/template.php
@@ -29,7 +29,7 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-inverse fixed-top" id="">
           <div class="container">
-            <a class="navbar-brand" href="index.php?action=listPosts">Mon blog</a>
+            <a class="navbar-brand" href="index.php?action=logon">Mon blog</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i class="fa fa-bars"></i>
